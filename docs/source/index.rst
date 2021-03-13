80551620111247a1d3ef148c4c853ba26d2f432a
Youngsung Kim <youngsun@ucar.edu>
2021-03-13 15:58:20 -0500

    test extraction for mpas ocn

---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. ekgen documentation master file, created by
+   sphinx-quickstart on Wed Mar 10 14:45:17 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ekgen's documentation!
+=================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- tests/cmdtest.sh
+++ tests/cmdtest.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/bash
 
 CASEDIR=/gpfs/alpine/cli115/proj-shared/grnydawn/e3sm_scratch/ERS_Ld5.T62_oQU120.CMPASO-NYF.summit_pgi.20210125_094617_kmwz7r
-#CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_advection_std.F
-CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_effective_density_in_land_ice.F
-OUTDIR=/ccs/home/grnydawn/scratch/kernels/ocn/ekgen_1
+#CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_diagnostics.F
+#ekgen_3 success CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/mode_forward/mpas_ocn_time_integration_split.F
+#ekgen_4 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_high_freq_thickness_hmix_del2.F
+#ekgen_5 success CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_surface_bulk_forcing.F
+#ekgen_6 success CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_vel_hadv_coriolis.F
+#ekgen_7 success CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_vel_vadv.F
+#ekgen_8 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tidal_potential_forcing.F
+#ekgen_9 error allocation? CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_vel_pressure_grad.F
+#ekgen_10 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_vel_hmix_del2.F
+#ekgen_12 success CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_thick_hadv.F
+#ekgen_13 derived type failure pooling CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_surface_bulk_forcing.F
+#ekgen_14 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_ecosys.F
+#ekgen_15 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_DMS.F
+#ekgen_16 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_MacroMolecules.F
+CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_surface_flux_to_tend.F
+OUTDIR=/ccs/home/grnydawn/scratch/kernels/ocn/ekgen_17
 
 ekgen mpasocn $CASEDIR $CALLSITEFILE -o $OUTDIR
