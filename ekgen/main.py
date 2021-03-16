ab9e95b5eea7b75fed8ac04832fd31035b7b374c
Youngsung Kim <youngsun@ucar.edu>
2021-03-16 19:19:39 -0400

    removed microapp from description

--- ekgen/main.py
+++ ekgen/main.py
@@ -6,9 +6,9 @@ from ekgen.eam import EAMKernel
 class E3SMKGen(Fortlab):
 
     _name_ = "ekgen"
-    _version_ = "0.1.0"
-    _description_ = "Microapp E3SM Fortran Kernel Generator"
-    _long_description_ = "Microapp E3SM Fortran Kernel Generator"
+    _version_ = "0.2.0"
+    _description_ = "E3SM Fortran Kernel Extraction and Analysis"
+    _long_description_ = "E3SM Fortran Kernel Extraction and Analysis"
     _author_ = "Youngsung Kim"
     _author_email_ = "youngsung.kim.act2@gmail.com"
     _url_ = "https://github.com/grnydawn/ekgen"
--- tests/cmdtest.sh
+++ tests/cmdtest.sh
@@ -3,6 +3,7 @@
 CASEDIR=/gpfs/alpine/cli115/proj-shared/grnydawn/e3sm_scratch/ERS_Ld5.T62_oQU120.CMPASO-NYF.summit_pgi.20210125_094617_kmwz7r
 #CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_diagnostics.F
 #ekgen_3 success CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/mode_forward/mpas_ocn_time_integration_split.F
+CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/mode_forward/mpas_ocn_time_integration_split.F
 #ekgen_4 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_high_freq_thickness_hmix_del2.F
 #ekgen_5 success CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_surface_bulk_forcing.F
 #ekgen_6 success CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_vel_hadv_coriolis.F
@@ -15,7 +16,8 @@ CASEDIR=/gpfs/alpine/cli115/proj-shared/grnydawn/e3sm_scratch/ERS_Ld5.T62_oQU120
 #ekgen_14 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_ecosys.F
 #ekgen_15 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_DMS.F
 #ekgen_16 no model CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_MacroMolecules.F
-CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_surface_flux_to_tend.F
-OUTDIR=/ccs/home/grnydawn/scratch/kernels/ocn/ekgen_17
+#CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/mpas-source/src/core_ocean/shared/mpas_ocn_tracer_surface_flux_to_tend.F
+#OUTDIR=/ccs/home/grnydawn/scratch/kernels/ocn/ekgen_17
+OUTDIR=/ccs/home/grnydawn/scrcli115/kernels/ocn/ekgen_release
 
 ekgen mpasocn $CASEDIR $CALLSITEFILE -o $OUTDIR
