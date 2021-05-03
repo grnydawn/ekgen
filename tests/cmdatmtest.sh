9a199104b27cef7fb7a647957167df3ca8dfa7c1
Youngsung Kim <youngsun@ucar.edu>
2021-05-03 14:38:29 -0400

    fixed scratch path to cli115

--- tests/cmdatmtest.sh
+++ tests/cmdatmtest.sh
@@ -2,6 +2,7 @@
 
 CASEDIR=/ccs/home/grnydawn/workbench/E3SM/case
 CALLSITEFILE=/ccs/home/grnydawn/repos/github/E3SM/components/cam/src/physics/cam/micro_mg_cam.F90
-OUTDIR=/ccs/home/grnydawn/scratch/kernels/eam/ekgen_eam1
+#OUTDIR=/ccs/home/grnydawn/scratch/kernels/eam/ekgen_eam1
+OUTDIR=/gpfs/alpine/cli115/proj-shared/grnydawn/e3sm_scratch/ekgen_eam1
 
 ekgen eam $CASEDIR $CALLSITEFILE -o $OUTDIR
